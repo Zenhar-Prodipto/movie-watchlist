607b036e66b9bfc03a822799770a1d7f5f31496d
Zenhar <zenhar.mz@gmail.com>
2025-04-16 23:56:29 +0600

    chore: Initialize React TypeScript project with Vite, Tailwind CSS, and Git setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 flex justify-center items-center">
+      <h1 className="text-3xl font-bold text-center text-blue-500">
+        Movie Watchlist App
+      </h1>
+    </div>
+  );
+};
+
+export default App;
